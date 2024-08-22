0d96b7fe6ee7a6535f95be417a2ea342a6fe5e97
Kevincango <kevincantugomez98@gmail.com>
2024-08-22 08:03:36 -0600

    adding images with JS

--- src/index.js
+++ src/index.js
@@ -3,4 +3,22 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+
+const imagesContainer = document.getElementById('images');
+const buttonAction = document.getElementById('generate-image');
+
+buttonAction.addEventListener('click', () => {
+    const singleImageContainer = document.createElement('div');
+    singleImageContainer.className = 'p-4';
+    const imageGenerate = document.createElement('img');
+    const randomInt = Math.floor(Math.random() * 124);
+    imageGenerate.loading = 'lazy';
+    imageGenerate.className = 'mx-auto';
+    imageGenerate.width = '320';
+    imageGenerate.src = `https://randomfox.ca/images/${randomInt}.jpg`;
+
+    singleImageContainer.appendChild(imageGenerate);
+    imagesContainer.appendChild(singleImageContainer);
+});
+
+
