b6df0cd7cd0168756d90f3eeb5c376ae67258770
Kevincango <kevincantugomez98@gmail.com>
2024-08-25 09:43:17 -0600

    clean button, img wrapper for slow loading and count variables for add and load

--- src/index.js
+++ src/index.js
@@ -5,23 +5,34 @@
 
 import {registerImage} from './jazy'
 
-
+let addImage = 0;
 const imagesContainer = document.getElementById('images');
+const divImageContainer = document.getElementsByClassName('bg-gray-300');
 const buttonAction = document.getElementById('generate-image');
 
 buttonAction.addEventListener('click', () => {
     const singleImageContainer = document.createElement('div');
-    singleImageContainer.className = 'p-4';
+    singleImageContainer.className = 'bg-gray-300';
+    singleImageContainer.style.minHeight = '100px';
+    singleImageContainer.style.display = 'inline';
     const imageGenerate = document.createElement('img');
     const randomInt = Math.floor(Math.random() * 124);
     //imageGenerate.loading = 'lazy';
     imageGenerate.className = 'mx-auto';
     imageGenerate.width = '320';
     imageGenerate.dataset.src = `https://randomfox.ca/images/${randomInt}.jpg`;
-
     singleImageContainer.appendChild(imageGenerate);
     imagesContainer.appendChild(singleImageContainer);
     registerImage(singleImageContainer);
+    console.log(`imagenes agregadas ${addImage++}`);
+});
+
+const cleanButton = document.getElementById('clean-images');
+
+cleanButton.addEventListener('click', () => {
+    [...divImageContainer].forEach((child) => {
+        child.remove();
+    })
 });
 
 
--- src/jazy.js
+++ src/jazy.js
@@ -1,14 +1,15 @@
 
+let loadImages = 0;
 
 const observer = new IntersectionObserver((entries) => {
     entries.filter((entry) => {
         return entry.isIntersecting;
     }).forEach((entry) => {
-        console.log('image is intersected');
         const node = entry.target;
         const imagen = node.firstChild;
         const url = imagen.dataset.src;
         imagen.src = url;
+        console.log(`imagenes cargadas ${loadImages++}`);
         observer.unobserve(node);
     });
 });
